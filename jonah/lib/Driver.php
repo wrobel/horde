1b5043ba06aa8fcd56121ec054aec6b4892e169d
Ian Roth <ian@hot-rain.com>
2011-05-02 21:24:07 -0400

    Fix for adding stories.

--- jonah/lib/Driver.php
+++ jonah/lib/Driver.php
@@ -186,16 +186,15 @@ class Jonah_Driver
      */
     public function getStoryLink($channel, $story)
     {
-        if (!empty($story['url']) && empty($story['body'])) {
-            $url = $story['url'];
-        } elseif ((empty($story['url']) || !empty($story['body'])) &&
-            !empty($channel['channel_story_url'])) {
-            $url = $channel['channel_story_url'];
+        $story_url = $channel->get('story_url');
+        if ((empty($story['url']) || !empty($story['body'])) &&
+            !empty($story_url)) {
+            $url = $story_url;
         } else {
             $url = Horde::url('stories/view.php', true, -1)->add(array('channel_id' => '%c', 'id' => '%s'))->setRaw(false);
         }
         return new Horde_Url(str_replace(array('%25c', '%25s', '%c', '%s'),
-                                         array('%c', '%s', $channel['channel_id'], $story['id']),
+                                         array('%c', '%s', $channel->getName(), $story['id']),
                                          $url));
     }
 
